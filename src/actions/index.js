05d458dd11066743e7513bcf72e2b96a147610f7
balibou <benjamin.cherion@gmail.com>
2017-06-13 18:42:19 +0100

    adding redux logic for movie description

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,22 @@
 import axios from 'axios';
-import { GET_MOVIES } from '../constants';
+import { GET_MOVIES, GET_MOVIE } from '../constants';
 
 export const getMovies = () => (dispatch) => {
   axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US`)
       .then(data => {
         dispatch({
           type: GET_MOVIES,
+          payload: data
+        });
+      })
+};
+
+export const getMovie = (movieId) => (dispatch) => {
+  axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=en-US`)
+      .then(data => {
+        console.log(data);
+        dispatch({
+          type: GET_MOVIE,
           payload: data
         });
       })
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import '../styles/Movie.css'
 
 class Movie extends Component {
   render() {
