2ed96ea0791a6b62306feee7208a89e1cbd85e26
balibou <benjamin.cherion@gmail.com>
2017-06-14 00:49:41 +0100

    adding a navbar

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,41 @@
 import React from 'react';
-import logo from '../styles/logo.svg';
+import {Link} from 'react-router';
+
+import logo from '../Canal+.svg'
 import '../styles/App.css';
 
 const App = ({ children }) => (
   <div className="App">
-    <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2>Welcome to React</h2>
-    </div>
+    <nav className="nav has-shadow" id="top" style={{background: '#000000'}}>
+      <div className="container">
+        <div className="nav-left">
+          <Link to={'/'}>
+          <a className="nav-item">
+            <img src={logo} alt="Canal plus logo" />
+          </a>
+        </Link>
+        </div>
+        <span className="nav-toggle">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+        <div className="nav-right nav-menu">
+          <a className="nav-item is-tab is-active">
+            Home
+          </a>
+          <a className="nav-item is-tab">
+            Features
+          </a>
+          <a className="nav-item is-tab">
+            Team
+          </a>
+          <a className="nav-item is-tab">
+            Help
+          </a>
+        </div>
+      </div>
+    </nav>
     <div>
       { children }
     </div>
