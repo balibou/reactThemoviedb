be660e227ed20527256eabb77bd826423a265f4e
balibou <benjamin.cherion@gmail.com>
2017-06-14 00:24:53 +0100

    adding algorithm for rendering star rating

--- src/components/MovieDescription.js
+++ src/components/MovieDescription.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 
 class MovieDescription extends Component {
+  renderStarsRating(rating){
+    var floorRating = Math.floor(rating)
+    var restRating = rating - floorRating
+    var starAddingAttribute = [];
+
+    for(var i = 0; i < 10; i++){
+      if(i === floorRating && restRating >= 0.5) starAddingAttribute.push('-half-empty')
+      else if(i === floorRating && restRating <= 0.5) starAddingAttribute.push('-o')
+      else if(i > floorRating) starAddingAttribute.push('-o')
+      else starAddingAttribute.push('')
+    }
+    return starAddingAttribute.map((attribute, index) => {
+      return <i key={index} className={`fa fa-star${attribute} title is-5`} style={{color: "#ed6c63"}} />
+    })
+  }
   render() {
-    const { original_title, id, backdrop_path, vote_count, overview, release_date, homepage, budget } = this.props.movie
+    const { original_title, id, backdrop_path, vote_count, overview, release_date, homepage, budget, vote_average } = this.props.movie
     const backdropPoster = `https://image.tmdb.org/t/p/w500${backdrop_path}`
 
     return (
@@ -20,11 +35,7 @@ class MovieDescription extends Component {
                 <hr />
                 <br />
                 <p>
-                  <i className="fa fa-star title is-5" style={{color: "#ed6c63"}} />
-                  <i className="fa fa-star title is-5" style={{color: "#ed6c63"}} />
-                  <i className="fa fa-star-half-empty title is-5" style={{color: "#ed6c63"}} />
-                  <i className="fa fa-star title is-5" />
-                  <i className="fa fa-star title is-5" />
+                  {this.renderStarsRating(vote_average)}
                   <strong>{vote_count} Votes</strong>
                 </p>
                 <br />
